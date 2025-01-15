99b5c4795892bd2548014642271d940c2002d6a2
Rohan Dave <rohandewangdave@gmail.com>
2025-01-15 11:59:32 -0500

    Update Status.tsx

--- src/Kambaz/Courses/Home/Status.tsx
+++ src/Kambaz/Courses/Home/Status.tsx
@@ -4,21 +4,47 @@ export default function CourseStatus() {
       <h2>Course Status</h2>
       <table>
         <tr>
-          <td>
+          <td colSpan={2}>
             <button>Unpublish</button>
-          </td>
-          <td>
             <button>Import Existing Content</button>
           </td>
         </tr>
+        <tr>
+          <td colSpan={2}>
+            <button>Publish</button>
+          </td>
+        </tr>
+        <tr>
+          <td colSpan={2}>
+            <button>Import from Commons</button>
+          </td>
+        </tr>
+        <tr>
+          <td colSpan={2}>
+            <button>Choose Home Page</button>
+          </td>
+        </tr>
+        <tr>
+          <td colSpan={2}>
+            <button>View Course Stream</button>
+          </td>
+        </tr>
+        <tr>
+          <td colSpan={2}>
+            <button>New Announcement</button>
+          </td>
+        </tr>
+        <tr>
+          <td colSpan={2}>
+            <button>New Analytics</button>
+          </td>
+        </tr>
+        <tr>
+          <td colSpan={2}>
+            <button>View Course Notifications</button>
+          </td>
+        </tr>
       </table>
-      <button>Publish</button>
-      <button>Import from Commons</button>
-      <button>Choose Home Page</button>
-      <button>View Course Stream</button>
-      <button>New Announcement</button>
-      <button>New Analytics</button>
-      <button>View Course Notifications</button>
     </div>
   );
 }
