304df81970a0caac6c187ba4b5434e8adca64100
Rohan Dave <rohandewangdave@gmail.com>
2025-01-26 18:57:29 -0500

    before implementing people

--- src/Kambaz/Courses/Home/Status.tsx
+++ src/Kambaz/Courses/Home/Status.tsx
@@ -1,50 +1,48 @@
+import { MdDoNotDisturbAlt } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
+import { BiImport } from "react-icons/bi";
+import { LiaFileImportSolid } from "react-icons/lia";
+import { Button } from "react-bootstrap";
+
 export default function CourseStatus() {
   return (
-    <div id="wd-course-status">
+    <div id="wd-course-status" style={{ width: "350px" }}>
       <h2>Course Status</h2>
-      <table>
-        <tr>
-          <td colSpan={2}>
-            <button>Unpublish</button>
-            <button>Publish</button>
-          </td>
-        </tr>
-        <tr>
-          <td colSpan={2}>
-            <button>Import Existing Content</button>
-          </td>
-        </tr>
-        <tr>
-          <td colSpan={2}>
-            <button>Import from Commons</button>
-          </td>
-        </tr>
-        <tr>
-          <td colSpan={2}>
-            <button>Choose Home Page</button>
-          </td>
-        </tr>
-        <tr>
-          <td colSpan={2}>
-            <button>View Course Stream</button>
-          </td>
-        </tr>
-        <tr>
-          <td colSpan={2}>
-            <button>New Announcement</button>
-          </td>
-        </tr>
-        <tr>
-          <td colSpan={2}>
-            <button>New Analytics</button>
-          </td>
-        </tr>
-        <tr>
-          <td colSpan={2}>
-            <button>View Course Notifications</button>
-          </td>
-        </tr>
-      </table>
+      <div className="d-flex">
+        <div className="w-50 pe-1">
+          <Button variant="secondary" size="lg" className="w-100 text-nowrap ">
+            <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish
+          </Button>
+        </div>
+        <div className="w-50">
+          <Button variant="success" size="lg" className="w-100">
+            <FaCheckCircle className="me-2 fs-5" /> Publish
+          </Button>
+        </div>
+      </div>
+      <br />
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <BiImport className="me-2 fs-5" /> Import Existing Content
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-5" /> Choose Home Page
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-5" /> View Course Stream
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-5" /> Announcement
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-5" /> Analytics
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-5" /> View Course Notifications
+      </Button>
+      {/* Complete the rest of the buttons */}
     </div>
   );
 }
