6ed68520c91ef6ad4f1a2c5200f9de79972895f6
Rohan Dave <rohandewangdave@gmail.com>
2025-01-26 19:22:26 -0500

    sign in, sign up and profile

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,10 +1,40 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 export default function AccountNavigation() {
   return (
-    <div id="wd-account-navigation">
-      <Link to={`/Kambaz/Account/Signin`}> Signin </Link> <br />
-      <Link to={`/Kambaz/Account/Signup`}> Signup </Link> <br />
-      <Link to={`/Kambaz/Account/Profile`}> Profile </Link> <br />
+    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+      <NavLink
+        className={({ isActive }) =>
+          `list-group-item border border-0 ${
+            isActive ? "active text-black" : "text-danger"
+          }`
+        }
+        to={`/Kambaz/Account/Signin`}
+      >
+        Signin
+      </NavLink>
+      <br />
+      <NavLink
+        className={({ isActive }) =>
+          `list-group-item border border-0 ${
+            isActive ? "active text-black" : "text-danger"
+          }`
+        }
+        to={`/Kambaz/Account/Signup`}
+      >
+        Signup
+      </NavLink>
+      <br />
+      <NavLink
+        className={({ isActive }) =>
+          `list-group-item border border-0 ${
+            isActive ? "active text-black" : "text-danger"
+          }`
+        }
+        to={`/Kambaz/Account/Profile`}
+      >
+        Profile
+      </NavLink>
+      <br />
     </div>
   );
 }
--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -1,19 +1,33 @@
+import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+
 export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" className="wd-username" />
+      <Form.Control id="wd-username" placeholder="username" className="mb-2" />
       <br />
-      <input placeholder="password" type="password" className="wd-password" />
-      <br />
-      <input
-        placeholder="verify password"
+      <Form.Control
+        id="wd-password"
+        placeholder="password"
         type="password"
-        className="wd-password-verify"
+        className="mb-2"
       />
       <br />
-      <Link to="/Kambaz/Account/Profile"> Sign up </Link>
+      <Form.Control
+        id="wd-confirm-password"
+        placeholder="confirm password"
+        type="password"
+        className="mb-2"
+      />
+      <br />
+      <Link
+        id="wd-signup-btn"
+        to="/Kambaz/Account/Profile"
+        className="btn btn-primary w-100 mb-2"
+      >
+        Sign up
+      </Link>
       <br />
       <Link to="/Kambaz/Account/Signin">Sign in</Link>
     </div>
