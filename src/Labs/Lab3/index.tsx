ffb81bcc1bd71efa9eb36046154fbda29f4eefe3
Rohan Dave <rohandewangdave@gmail.com>
2025-02-13 16:32:46 -0500

    lab 3 work

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,80 @@
+import Add from "./Add";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import ArrowFunctions from "./ArrowFunctions";
+import BooleanVariables from "./BooleanVariables";
+import Classes from "./Classes";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import Destructing from "./Destructuring";
+import DestructingImports from "./DestructuringImports";
+import FilterFunction from "./FilterFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
+import FunctionDestructing from "./FunctionDestructuring";
+import House from "./House";
+import IfElse from "./IfElse";
+import JsonStringify from "./JsonStringify";
+// import ImpliedReturn from "./ImpliedReturn";
+import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
+import SimpleArrays from "./SimpleArrays";
+import Spreading from "./Spreading";
+import Square from "./Square";
+import Styles from "./Styles";
+import TemplateLiterals from "./TemplateLiterals";
+import TernaryOperator from "./TernaryOperator";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import Highlight from "./Highlight";
+import PathParameters from "./PathParameters";
+
 export default function Lab3() {
+  console.log("Hello World!");
+
   return (
-    <div>
-      <h2>Lab 3</h2>
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      {/* This does not work */}
+      {/* <ImpliedReturn /> */}
+      <TemplateLiterals />
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoItem />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={3} b={4} />
+      <Square>4</Square>
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione
+        eaque illo minus cum, saepe totam vel nihil repellat nemo explicabo
+        excepturi consectetur. Modi omnis minus sequi maiores, provident
+        voluptates.
+      </Highlight>
+      <PathParameters />
     </div>
   );
 }
